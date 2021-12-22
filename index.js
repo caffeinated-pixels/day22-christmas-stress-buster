416f3db03e213926e8e11bb2f3c5cbe09e835be1
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-22 09:10:52 -0500

    Add basic timer functionality

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const btn = document.getElementById('btn')
 let timer = document.getElementById('timer')
 let affirmation = document.getElementById('affirmation')
-let timeout
+
+btn.addEventListener('click', startStopTimer)
+
+let timeLeft = 900
+let isTimerRunning = false
+let intervalID = null
+
+function startStopTimer() {
+  if (!isTimerRunning) {
+    console.log('start timer')
+    intervalID = setInterval(() => {
+      timeLeft--
+      console.log(timeLeft)
+    }, 1000)
+    isTimerRunning = true
+  } else {
+    console.log('pause timer')
+    clearInterval(intervalID)
+    isTimerRunning = false
+  }
+}
 
 // Task:
 // - Write a function to start the countdown timer on the button click and display it in the DOM. Stop the timer when it reaches 0.
